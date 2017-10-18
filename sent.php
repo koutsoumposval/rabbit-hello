011be2b1a73fe34917148be6188c0060335cc1a0
Valantis Koutsoumpos <chrysovalantis.koutsoumpos@devmob.com>
2017-10-18 21:49:43 +0200

    [App] Refactored send + receive scripts

--- sent.php
+++ sent.php
@@ -4,14 +4,28 @@ require_once __DIR__ . '/vendor/autoload.php';
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 
-$connection = new AMQPStreamConnection('rabbit-hello.dev', 5672, 'guest', 'guest');
+$connection = new AMQPStreamConnection(
+    'rabbit-hello.dev',
+    5672,
+    'guest',
+    'guest'
+);
+
 $channel = $connection->channel();
 
+$channel->queue_declare(
+    'hello',
+    false,
+    false,
+    false,
+    false
+);
 
-$channel->queue_declare('hello', false, false, false, false);
-
-$msg = new AMQPMessage('Hello World!');
-$channel->basic_publish($msg, '', 'hello');
+$channel->basic_publish(
+    new AMQPMessage('Hello World!'),
+    '',
+    'hello'
+);
 
 echo " [x] Sent 'Hello World!'\n";
 
